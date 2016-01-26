ac8c3a11c8c04d11090df1333d5b007951ad6a88
abrahimladha <abrahimladha@protonmail.ch>
2016-01-25 22:57:31 -0500

    init

---
+++ AI_PROJ1.java
@@ -0,0 +1,165 @@
+import javafx.scene.Scene;
+
+import javafx.application.Application;
+import javafx.beans.property.*;
+import javafx.beans.value.*;
+import javafx.collections.*;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.stage.Stage;
+
+/** Drag the anchors around to change a polygon's points. */
+public class AI_PROJ1 extends Application {
+  public static void main(String[] args) throws Exception { launch(args); }
+
+  // main application layout logic.
+  @Override public void start(final Stage stage) throws Exception {
+    Polygon triangle = createStartingTriangle();
+    Polygon triangle1 = createSecondTriangle();
+    Group root = new Group();
+    root.getChildren().add(triangle);
+    root.getChildren().addAll(createControlAnchorsFor(triangle.getPoints()));
+    root.getChildren().add(triangle1);
+    root.getChildren().addAll(createControlAnchorsFor(triangle1.getPoints()));
+    stage.setTitle("AI_PROJ1");
+    stage.setScene(
+        new Scene(
+            root,
+            400, 400, Color.ALICEBLUE
+        )
+    );
+    stage.show();
+  }
+
+  // creates a triangle.
+  private Polygon createStartingTriangle() {
+    Polygon triangle = new Polygon();
+
+    triangle.getPoints().setAll(
+        100d, 50d,
+        100d, 100d,
+        100d, 150d,
+        150d, 150d,
+        150d, 100d
+    );
+
+    triangle.setStroke(Color.BLUE);
+    triangle.setStrokeWidth(4);
+    triangle.setStrokeLineCap(StrokeLineCap.ROUND);
+    triangle.setFill(Color.BLUE);
+    return triangle;
+  }
+
+  private Polygon createSecondTriangle() {
+    Polygon triangle = new Polygon();
+    triangle.getPoints().setAll(
+        400d, 400d,
+        400d, 425d,
+        425d, 425d
+    );
+
+    triangle.setStroke(Color.BLUE);
+    triangle.setStrokeWidth(4);
+    triangle.setStrokeLineCap(StrokeLineCap.ROUND);
+    triangle.setFill(Color.BLUE);
+    return triangle;
+  }
+
+  // @return a list of anchors which can be dragged around to modify points in the format [x1, y1, x2, y2...]
+  private ObservableList<Anchor> createControlAnchorsFor(final ObservableList<Double> points) {
+    ObservableList<Anchor> anchors = FXCollections.observableArrayList();
+
+    for (int i = 0; i < points.size(); i+=2) {
+      final int idx = i;
+
+      DoubleProperty xProperty = new SimpleDoubleProperty(points.get(i));
+      DoubleProperty yProperty = new SimpleDoubleProperty(points.get(i + 1));
+
+      xProperty.addListener(new ChangeListener<Number>() {
+        @Override public void changed(ObservableValue<? extends Number> ov, Number oldX, Number x) {
+          points.set(idx, (double) x);
+        }
+      });
+
+      yProperty.addListener(new ChangeListener<Number>() {
+        @Override public void changed(ObservableValue<? extends Number> ov, Number oldY, Number y) {
+          points.set(idx + 1, (double) y);
+        }
+      });
+
+      anchors.add(new Anchor(Color.BLACK, xProperty, yProperty));
+    }
+
+    return anchors;
+  }
+
+  // a draggable anchor displayed around a point.
+  class Anchor extends Circle {
+    private final DoubleProperty x, y;
+
+    Anchor(Color color, DoubleProperty x, DoubleProperty y) {
+      super(x.get(), y.get(), 10);
+      setFill(color);
+      setStroke(color);
+      setStrokeWidth(3);
+      setStrokeType(StrokeType.OUTSIDE);
+
+      this.x = x;
+      this.y = y;
+
+      x.bind(centerXProperty());
+      y.bind(centerYProperty());
+      enableDrag();
+    }
+
+    // make a node movable by dragging it around with the mouse.
+    private void enableDrag() {
+      final Delta dragDelta = new Delta();
+      setOnMousePressed(new EventHandler<MouseEvent>() {
+        @Override public void handle(MouseEvent mouseEvent) {
+          // record a delta distance for the drag and drop operation.
+          dragDelta.x = getCenterX() - mouseEvent.getX();
+          dragDelta.y = getCenterY() - mouseEvent.getY();
+          getScene().setCursor(Cursor.MOVE);
+        }
+      });
+      setOnMouseReleased(new EventHandler<MouseEvent>() {
+        @Override public void handle(MouseEvent mouseEvent) {
+          getScene().setCursor(Cursor.HAND);
+        }
+      });
+      setOnMouseDragged(new EventHandler<MouseEvent>() {
+        @Override public void handle(MouseEvent mouseEvent) {
+          double newX = mouseEvent.getX() + dragDelta.x;
+          if (newX > 0 && newX < getScene().getWidth()) {
+            setCenterX(newX);
+          }
+          double newY = mouseEvent.getY() + dragDelta.y;
+          if (newY > 0 && newY < getScene().getHeight()) {
+            setCenterY(newY);
+          }
+        }
+      });
+      setOnMouseEntered(new EventHandler<MouseEvent>() {
+        @Override public void handle(MouseEvent mouseEvent) {
+          if (!mouseEvent.isPrimaryButtonDown()) {
+            getScene().setCursor(Cursor.HAND);
+          }
+        }
+      });
+      setOnMouseExited(new EventHandler<MouseEvent>() {
+        @Override public void handle(MouseEvent mouseEvent) {
+          if (!mouseEvent.isPrimaryButtonDown()) {
+            getScene().setCursor(Cursor.DEFAULT);
+          }
+        }
+      });
+    }
+
+    // records relative x and y co-ordinates.
+    private class Delta { double x, y; }
+  }
+}

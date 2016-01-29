27bf53d3cb85c11d1c16d42539299664edf1b0d3
abrahimladha <abrahimladha@protonmail.ch>
2016-01-28 20:32:00 -0500

    added convex hull, but unsure if its working right

--- AI_PROJ1.java
+++ AI_PROJ1.java
@@ -11,6 +11,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.stage.Stage;
+import javafx.geometry.Point2D;
 
 /** Drag the anchors around to change a polygon's points. */
 public class AI_PROJ1 extends Application {
@@ -29,8 +30,8 @@ public class AI_PROJ1 extends Application {
      //= new virtualToRealPolygon(triangle.getPoints());
     //List<Double> vtriangletestlist = virtualToRealPolygon(triangle.getPoints().toList().toBlocking().single());
     Polygon vtriangle = new Polygon();
-    vtriangle.getPoints().setAll(virtualToRealPolygon(triangle.getPoints()));
-    //vtriangle.setStroke(Color.BLUE);
+    vtriangle.getPoints().setAll(getConvexHull(virtualToRealPolygon(triangle.getPoints())));
+    vtriangle.setStroke(Color.BLUE);
     vtriangle.setStrokeWidth(1);
     root.getChildren().addAll(vtriangle);
     stage.setTitle("AI_PROJ1");
@@ -42,6 +43,71 @@ public class AI_PROJ1 extends Application {
     );
     stage.show();
   }
+  private ObservableList<Double> getConvexHull(ObservableList<Double> s){
+    Point2D[] myPoints = new Point2D[s.size()/2];
+    for(int i = 0; i < myPoints.length; i++)
+        myPoints[i] = new Point2D(s.get(2*i),s.get((2*i)+1));
+
+
+
+      Point2D h0 = getRightMostLowestPoint(myPoints);
+      ArrayList<Point2D> H = new ArrayList<Point2D>();
+      H.add(h0);
+      Point2D t0 = h0;
+
+      while (true) {
+        Point2D t1 = myPoints[0];
+        for(int i = 1; i < myPoints.length; i++){
+            double status = whichSide(t0.getX(), t0.getY(), t1.getX(), t1.getY(), myPoints[i].getX(), myPoints[i].getY());
+            if(status > 0)
+                t1 = myPoints[i];
+            else  if (status == 0){
+                if(distance(myPoints[i].getX(), myPoints[i].getY(), t0.getX(), t0.getY()) > distance(t1.getX(), t1.getY(), t0.getX(), t0.getY()))
+                    t1 = myPoints[i];
+            } 
+        }
+      if(t1.getX() == h0.getX() && t1.getY() == h0.getY())
+          break;
+      else {
+        H.add(t1);
+        t0 = t1;
+      }
+   }
+    ObservableList<Double> pnts = FXCollections.observableArrayList();
+    for(int j = 0; j < H.size(); j++){
+        pnts.add(myPoints[j].getX());
+        pnts.add(myPoints[j].getY());
+    }
+    return pnts;
+  }
+  //}
+  public double whichSide(double x0, double y0, double x1, double y1, double x2, double y2){
+    return (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0);
+  }
+  public double distance(double x1, double y1, double x2, double y2) {
+  return Math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+  }
+
+  static Point2D getRightMostLowestPoint(Point2D[] p){
+    int rightMostIndex = 0;
+    double rightMostX = p[0].getX();
+    double rightMostY = p[0].getY();
+    for(int i = 0; i < p.length; i++){
+        if(rightMostY < p[i].getY()) {
+            rightMostY = p[i].getY();
+            rightMostX = p[i].getX();
+            rightMostIndex = i;
+        }
+        else if (rightMostY == p[i].getY() && rightMostX < p[i].getX()){
+            rightMostX = p[i].getX();
+            rightMostIndex = i;
+        }
+        
+    }
+    return p[rightMostIndex];
+  }
+  
+
 
   // creates a triangle.
   private Polygon createStartingTriangle() {
@@ -62,13 +128,10 @@ public class AI_PROJ1 extends Application {
   private ObservableList<Double> virtualToRealPolygon(final ObservableList<Double> rpoints){
    ObservableList<Double> vpoints = FXCollections.observableArrayList();
     for(int i = 0; i < rpoints.size(); i+= 2){
-        vpoints.add(rpoints.get(i) + 86.6);
-        vpoints.add(rpoints.get(i+1) + 50.0);
+        vpoints.add(rpoints.get(i) + 100.0);
+        vpoints.add(rpoints.get(i+1) + 100.0);
         
         vpoints.add(rpoints.get(i));
-        //vpoints.add(rpoints.get(i));
-
-        //vpoints.add(rpoints.get(i+1));
         vpoints.add(rpoints.get(i+1));
     }
     return vpoints;
@@ -82,7 +145,8 @@ public class AI_PROJ1 extends Application {
     triangle.getPoints().setAll(
         400d, 400d,
         400d, 425d,
-        425d, 425d
+        425d, 425d,
+        600d, 600d
     );
 
     triangle.setStroke(Color.BLUE);
